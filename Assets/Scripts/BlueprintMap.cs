2408ffc5bce9a5e2e57f937017f00d6183f646ed
Christopher Williams <c.j.williams717@gmail.com>
2018-02-24 13:46:25 -0500

    AI makes blueprints
    Will work on scoring of blueprint moves next if everything is ready to go

---
+++ Assets/Scripts/BlueprintMap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueprintMap
+{
+    public Blueprint blueprint;
+    public HashSet<Coord> missingCoords;
+    public Coord targetCoord;
+    public int rotations;
+
+    public BlueprintMap()
+    {
+        blueprint = null;
+        missingCoords = null;
+        targetCoord = new Coord();
+        rotations = 0;
+    }
+
+    public BlueprintMap(Blueprint _blueprint, HashSet<Coord> _missingCoords, Coord _coord, int _rotations)
+    {
+        blueprint = _blueprint;
+        missingCoords = _missingCoords;
+        targetCoord = _coord;
+        rotations = _rotations;
+    }
+}
