bd0ddd4b00305291a0bdbd9d3c00471733df6091
Jack Schlesinger <jackschlesinger@gmail.com>
2018-06-12 12:24:42 -0400

    Reorganized audio.

--- Assets/Scripts/Audio/ClipLibrary.cs
+++ Assets/Scripts/Audio/ClipLibrary.cs
@@ -26,17 +26,11 @@ public class ClipLibrary : ScriptableObject {
     [SerializeField] private AudioClip prodLevelUp;
     public AudioClip ProdLevelUp { get { return prodLevelUp; } }
 
-    [SerializeField] private AudioClip[] playAvailable;
-    public AudioClip[] PlayAvailable { get { return playAvailable; } }
-
     [SerializeField] private AudioClip structureClaimed;
     public AudioClip StructureClaimed { get { return structureClaimed; } }
 
     [SerializeField] private AudioClip resourceGained;
     public AudioClip ResourceGained { get { return resourceGained; } }
-
-    [SerializeField] private AudioClip blockConstructed;
-    public AudioClip BlockConstructed { get { return blockConstructed; } }
 
     [SerializeField] private AudioClip illegalPlay;
     public AudioClip IllegalPlay { get { return illegalPlay; } }
