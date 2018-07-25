db4ded24816ebe0584f011ee3d18ec6ce9ad2eee
Jack Schlesinger <jackschlesinger@gmail.com>
2018-07-25 10:36:39 -0400

    Added loading animation.

--- Assets/Scripts/LoadingScreenController.cs
+++ Assets/Scripts/LoadingScreenController.cs
@@ -7,36 +7,14 @@ using UnityEngine.Rendering;
 
 public class LoadingScreenController : MonoBehaviour
 {
-
-	public TextMeshProUGUI loadingText;
 	private bool load_started = false;
 
-	void Awake() {
-	}
-	
-	// Update is called once per frame
 	void Update () {
 		if ((!load_started) && (SplashScreen.isFinished))
 		{
 			StartCoroutine(LoadGameAsync());
 			load_started = true;
 		}
-		
-		switch (Mathf.FloorToInt(Time.time) % 4)
-		{
-			case (0) :
-				loadingText.text = "LOADING";
-				break;
-			case (1) :
-				loadingText.text = "LOADING.";
-				break;
-			case (2) :
-				loadingText.text = "LOADING..";
-				break;
-			case (3) :
-				loadingText.text = "LOADING...";
-				break;
-		}	
 	}
 
 	IEnumerator LoadGameAsync()
